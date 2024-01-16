adfbb843be2f3f63ec409a7516536dc96f4647f4
shaselton-usds <shaselton.usds@gmail.com>
2024-01-16 10:16:52 -0800

    moving json tests to version folders

---
+++ test/1.1/json.spec.ts
@@ -0,0 +1,39 @@
+import test from "ava"
+import { loadFixtureStream } from "../utils.js"
+import { validateJson } from "../../src/json.js"
+
+test("validateJson", async (t) => {
+  const result = await validateJson(loadFixtureStream("sample-1.json"), "v1.1")
+  t.is(result.valid, false)
+  t.is(result.errors.length, 1)
+})
+
+test("validateJson empty", async (t) => {
+  const result = await validateJson(
+    loadFixtureStream("sample-empty.json"),
+    "v1.1"
+  )
+  t.is(result.valid, false)
+  t.deepEqual(result.errors.length, 4)
+})
+
+test("validateJson maxErrors", async (t) => {
+  const result = await validateJson(
+    loadFixtureStream("sample-empty.json"),
+    "v1.1",
+    {
+      maxErrors: 1,
+    }
+  )
+  t.is(result.valid, false)
+  t.deepEqual(result.errors.length, 1)
+})
+
+test("validateJson valid", async (t) => {
+  const result = await validateJson(
+    loadFixtureStream("sample-valid.json"),
+    "v1.1"
+  )
+  t.is(result.valid, true)
+  t.deepEqual(result.errors.length, 0)
+})
