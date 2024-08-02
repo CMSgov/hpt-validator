c42567780ad441a9cae399623dc673483c30f94c
shaselton-usds <shaselton.usds@gmail.com>
2024-08-02 14:42:15 -0700

    adding a protective check for builtin string functions

--- test/common/csv.spec.ts
+++ test/common/csv.spec.ts
@@ -1,5 +1,5 @@
 import test from "ava"
-import { csvColumnName } from "../../src/versions/common/csv.js"
+import { csvColumnName, matchesString } from "../../src/versions/common/csv.js"
 
 test("csvColumnName", (t) => {
   t.is(csvColumnName(0), "A")
@@ -7,4 +7,11 @@ test("csvColumnName", (t) => {
   t.is(csvColumnName(702), "AAA")
   t.is(csvColumnName(18278), "AAAA")
   t.is(csvColumnName(475254), "AAAAA")
+})
+
+test("matchesString", (t) => {
+  t.is(matchesString("testing", "testing"), true)
+  t.is(matchesString("testing", "tresting"), false)
+  t.is(matchesString(1 as unknown as string, "testing"), false)
+  t.is(matchesString(undefined as unknown as string, "testing"), false)
 })
