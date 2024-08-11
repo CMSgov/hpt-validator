629886a4984847e59ece28a35bc0f707b78c1c72
Mint Thompson <mathompson@mitre.org>
2024-08-11 11:22:36 -0400

    Remove BOM from JSON stream
    When JSON input is a readable stream, a byte-order mark may be present
    at the beginning of the stream. When reading the first chunk of the
    stream, check for the presence of the byte-order mark. Remove it if it
    is present.

--- src/versions/common/json.ts
+++ src/versions/common/json.ts
@@ -25,12 +25,28 @@ export async function parseJson(
 
     readChunk()
   } else {
+    let firstChunk = true
     const jsonStream = jsonInput as NodeJS.ReadableStream
     jsonStream.on("end", () => parser.end())
     jsonStream.on("error", (e) => {
       throw e
     })
-    jsonStream.on("data", (data) => parser.write(data))
+    jsonStream.on("data", (data: string) => {
+      // strip utf-8 BOM: see https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8
+      if (firstChunk) {
+        const dataBuffer = Buffer.from(data)
+        if (
+          dataBuffer.length > 2 &&
+          dataBuffer[0] === 0xef &&
+          dataBuffer[1] === 0xbb &&
+          dataBuffer[2] === 0xbf
+        ) {
+          data = data.trimStart()
+        }
+        firstChunk = false
+      }
+      parser.write(data)
+    })
   }
 }
 
